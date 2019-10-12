a2fb2ae55f21975855cf3d084c2559e78090700d
Denvinn Magsino <lambda_school_loaner_170@Lambda-School-Loaner-170s-MacBook-Pro.local>
2019-10-12 10:46:28 -0700

    can login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
-
+import React, {useState} from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 const Login = () => {
+
+  const [ login, setLogin ] = useState({
+    username:'',
+    password:''
+  })
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const changeHandler = e => {
+    e.persist();
+
+    let value = e.target.value
+
+    setLogin({
+      ...login,
+      [e.target.name] : value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(login)
+    axiosWithAuth()
+      .post('/login', login)
+      .then(res => {
+        console.log(res)
+        localStorage.setItem("token", res.data.payload);
+      })
+      .catch(err => console.log(err))
+
+  }
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="username"
+          type="text"
+          placeholder="login"
+          onChange={changeHandler}
+          value={login.username}
+        />
+        <input 
+          name="password"
+          type="password"
+          placeholder="password"
+          onChange={changeHandler}
+          value={login.password}
+        />
+        <button>
+          submit
+        </button>
+      </form>
     </>
   );
 };
